f6102b9b12b348109c4792fea1c8a462e505a7d0
Christian Bailey <cmbail@users.noreply.github.com>
2025-12-31 08:33:44 -0800

    optimized

--- final_challenge.py
+++ final_challenge.py
@@ -1,23 +1,26 @@
-# Final Challenge: The "Library Management System"
-class Item:
-    def __init__(self, title, id_number):
-        self.title = title
-        self.id_number = id_number
-    def get_info(self):
-        return f'ID: {self.id_number} - Title: {self.title}'
+from dataclasses import dataclass
 
-class Book(Item):
-    def __init__(self, title, id_number, author):
-        super().__init__(title, id_number)
-        self.author = author
+@dataclass
+class Item:
+    title: str
+    id_number: int
+
     def get_info(self):
-        return f'ID: {self.id_number} - Book: {self.title} by {self.author}'
+        return f"ID: {self.id_number} - Title: {self.title}"
+
+@dataclass
+class Book(Item):
+    author: str
+
+    def get_info(self):
+        return f"ID: {self.id_number} - Book: {self.title} by {self.author}"
+
 
 library = [
     Item("Library Map", 101),
     Book("The Great Gatsby", 202, "F. Scott Fitzgerald"),
-    Book("1984", 303, "George Orwell")
+    Book("1984", 303, "George Orwell"),
 ]
 
-for i in library:
-    print(i.get_info())
+for item in library:
+    print(item.get_info())
